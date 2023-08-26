381dfbcc2103f0ac13e8b20c520b163900cc1b1d
abubeker <abubekera87@gmail.com>
2023-08-26 14:57:16 +0300

    displaying icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { IconType } from "react-icons";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstaion: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          color="gray.500"
+          as={iconMap[platform.slug]}
+        ></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
