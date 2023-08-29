682dfd8303fc46cbab60deafb5b9b0620cedf71c
abubeker <abubekera87@gmail.com>
2023-08-29 11:45:04 +0300

    built search element

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/—Pngtree—3d logo design vector cdr_5883019.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const Navbar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
